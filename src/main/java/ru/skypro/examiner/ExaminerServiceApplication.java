181d9f6fe1c55003b4a1150a79e09a61a14c22ce
hp <747685@mail.ru>
2023-07-10 21:54:17 +0300

    course work 2

---
+++ src/main/java/ru/skypro/examiner/ExaminerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.examiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/examiner/ExaminerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.examiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
